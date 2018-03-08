1cadf6085b10f62e9b89901f6f1ac96199b220d3
eslem <j.eslem03@gmail.com>
2018-03-08 22:12:25 +0100

    fixed export templates

--- src/app/controllers/index.ts
+++ src/app/controllers/index.ts
@@ -1,2 +1,4 @@
 export { LoopbackApiLoaderComponent } from './impl/loopback-api-loader/loopback-api-loader.component';
 export { LoopbackApiItemEditorComponent } from './impl/loopback-api-item-editor/loopback-api-item-editor.component';
+
+export * from './templates';
--- src/app/controllers/templates/index.ts
+++ src/app/controllers/templates/index.ts
@@ -1,1 +1,2 @@
 export { apiLoaderTemplate } from './api-loader-template';
+export { apiModelEditTemplate } from './api-item-editor';
